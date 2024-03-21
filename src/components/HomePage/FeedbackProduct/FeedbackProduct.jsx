0153e71b2638bf82e82a126d782260e3c120f7a6
GagoPalyan <134396565+GagoPalyan@users.noreply.github.com>
2024-03-21 22:46:28 +0400

    feedback product

--- src/components/HomePage/FeedbackProduct/FeedbackProduct.jsx
+++ src/components/HomePage/FeedbackProduct/FeedbackProduct.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
 import Star from "../../../sharedComponents/Star/Star";
 import Title from "../../../sharedComponents/Title/Title";
 import style from "./FeedbackProduct.module.css";
 import FeedbackProductList from "./FeedbackProductList/FeedbackProductList";
 
 function FeedbackProduct() {
+  const [active, setActive] = useState(2);
+  const [data, setData] = useState(FeedbackProductList);
+
+  const change = (item) => {
+    setActive(item.id);
+    let dataLocal = [data.find((el) => el.id == item.id)];
+    let itemindex = data.findIndex((el) => el.id == item.id);
+    dataLocal.push(data[itemindex + 1 > data.length - 1 ? 0 : itemindex + 1]);
+    dataLocal.unshift(
+      data[itemindex - 1 < 0 ? data.length - 1 : itemindex - 1]
+    );
+    setData(dataLocal);
+  };
   return (
     <section className={style.content}>
       <Title caption="reviwe" text="Customer feedback on the product" />
       <div className={style.carusel}>
-        {FeedbackProductList.map((item) => {
+        {data.map((item) => {
           return (
             <div
               key={item.id}
-              className={item.active ? style.active : style.cart}
+              className={active == item.id ? style.active : style.cart}
             >
               <div className={style.stars}>
                 {item.stars.map((el, index) => {
@@ -21,7 +35,7 @@ function FeedbackProduct() {
                       key={index}
                       starFill={true}
                       star={
-                        item.active
+                        active == item.id
                           ? { width: "17", height: "14" }
                           : { width: "11", height: "10" }
                       }
@@ -46,12 +60,13 @@ function FeedbackProduct() {
         })}
       </div>
       <div className={style.buttons}>
-        {FeedbackProductList.map((item, index) => {
+        {data.map((item, index) => {
           return (
             <button
               key={index}
-              style={item.active ? { backgroundColor: "#afb293" } : null}
+              style={active == item.id ? { backgroundColor: "#afb293" } : null}
               className={style.Btn}
+              onClick={() => change(item)}
             ></button>
           );
         })}

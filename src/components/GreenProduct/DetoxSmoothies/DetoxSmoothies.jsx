7049f7f42d404b143a3d052257df09c0694b351d
GagoPalyan <134396565+GagoPalyan@users.noreply.github.com>
2024-04-17 17:41:29 +0400

    Mobile GreenProduct Page is ready

--- src/components/GreenProduct/DetoxSmoothies/DetoxSmoothies.jsx
+++ src/components/GreenProduct/DetoxSmoothies/DetoxSmoothies.jsx
@@ -13,6 +13,8 @@ import Title from "../../../sharedComponents/Title/Title";
 import Cart from "../../../sharedComponents/Cart/Cart";
 import DetoxSmoothiesFilter from "./DetoxSmoothiesFilter/DetoxSmoothiesFilter";
 import { searchFunc } from "../../../utils/search";
+import Media from "react-media";
+import matchers from "@testing-library/jest-dom/matchers";
 
 function DetoxSmoothies() {
   const products = useSelector(selectProducts);
@@ -41,20 +43,37 @@ function DetoxSmoothies() {
   }, [search]);
 
   return (
-    <section className={style.content}>
-      <Title
-        caption="Detox Smoothies"
-        text="Cold Infusion And Pressed Detox Juice"
-      />
-      <DetoxSmoothiesFilter />
-      <div className={style.list}>
-        {list.length > 0 ? (
-          list.map((item) => <Cart key={item.id} item={item} />)
+    <Media query={"(max-width: 992px)"}>
+      {(matchers) => {
+        return !matchers ? (
+          <section className={style.content}>
+            <Title
+              caption="Detox Smoothies"
+              text="Cold Infusion And Pressed Detox Juice"
+            />
+            <DetoxSmoothiesFilter />
+            <div className={style.list}>
+              {list.length > 0 ? (
+                list.map((item) => <Cart key={item.id} item={item} />)
+              ) : (
+                <h1>There are currently no products available</h1>
+              )}
+            </div>
+          </section>
         ) : (
-          <h1>There are currently no products available</h1>
-        )}
-      </div>
-    </section>
+          <section className={style.content}>
+            <DetoxSmoothiesFilter />
+            <div className={style.list}>
+              {list.length > 0 ? (
+                list.map((item) => <Cart key={item.id} item={item} />)
+              ) : (
+                <h1>There are currently no products available</h1>
+              )}
+            </div>
+          </section>
+        );
+      }}
+    </Media>
   );
 }
 
--- src/components/GreenProduct/GreenProduct.jsx
+++ src/components/GreenProduct/GreenProduct.jsx
@@ -1,13 +1,24 @@
 import DetoxSmoothiesImg from "./DetoxSmoothiesImg/DetoxSmoothiesImg";
 import DetoxSmoothies from "./DetoxSmoothies/DetoxSmoothies";
 import style from "./GreenProduct.module.css";
+import Media from "react-media";
+import { queries } from "@testing-library/react";
+import GreenProductMobile from "../../mobile/GreenProductMobile/GreenProductMobile";
 
 function GreenProduct() {
   return (
-    <main className={style.content}>
-      <DetoxSmoothiesImg />
-      <DetoxSmoothies />
-    </main>
+    <Media query={"(max-width: 992px)"}>
+      {(matchers) => {
+        return !matchers ? (
+          <main className={style.content}>
+            <DetoxSmoothiesImg />
+            <DetoxSmoothies />
+          </main>
+        ) : (
+          <GreenProductMobile />
+        );
+      }}
+    </Media>
   );
 }
 
